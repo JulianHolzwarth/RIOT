6f9934e761477004ca25049a457d73dff05dc321
JulianHolzwarth <legolas1509@googlemail.com>
2019-04-17 17:05:29 +0200

    task_test for new delete

--- tests/freertos/task_test.c
+++ tests/freertos/task_test.c
@@ -25,6 +25,8 @@ typedef struct {
     char word2;
 } paramStruct;
 
+paramStruct global_xParameter;
+
 /**
  * @brief   tests the freertos task
  *
@@ -36,29 +38,16 @@ int task_test_start(void)
 
     mutex_init(&test_mutex);
 
-    paramStruct *xParameter = (paramStruct *)malloc(sizeof(paramStruct));
+    paramStruct *xParameter = &global_xParameter;
     xParameter->word1 = 'A';
     xParameter->word2 = 'B';
     TaskHandle_t xHandle;
     xTaskCreate( task_test_thread, "testing task", THREAD_STACKSIZE_DEFAULT, (void *)xParameter, THREAD_PRIORITY_MAIN, &xHandle );
-
-    mutex_unlock(&test_mutex);
     vTaskDelay(portTICK_RATE_MS(10));
-    /* mutex gets locked by thread while this thread is delayed */
-    if (mutex_trylock(&test_mutex) == true) {
-        puts("task did not lock");
-        test_result = pdFAIL;
-    }
     uint32_t pid = (uint32_t )xHandle;
     thread_t *thread = (thread_t *)sched_threads[pid];
-    if (thread->status == STATUS_STOPPED) {
-        puts("thread stopped before is should have");
-        test_result = pdFAIL;
-    }
-    vTaskDelete(xHandle);
-    vTaskDelay(20);
     if (thread->status != STATUS_STOPPED) {
-        puts("thread did not stop after getting deleted");
+        puts("thread did not stop after it deleted itself");
         test_result = pdFAIL;
     }
 
@@ -92,14 +81,15 @@ void task_test_thread(void *pvParameters)
     paramStruct pxParameters = *( paramStruct * )pvParameters;
 
     if (pxParameters.word1 !=  'A'  || pxParameters.word2 != 'B') {
-        mutex_unlock(&test_mutex);
+        mutex_trylock(&test_mutex);
         puts("wrong parameter");
         return;
     }
-    /* runs until the thread gets deleted */
-    while (true) {
-        mutex_trylock(&test_mutex);
-        vTaskDelay(10);
-        thread_yield();
-    }
+}
+
+void task_delete_test(void *pvParameter)
+{
+    (void)pvParameter;
+    vTaskDelete(NULL);
+    thread_sleep();
 }
