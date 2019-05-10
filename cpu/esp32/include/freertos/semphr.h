806cb90aabdb34e55d2e2ac181eed8055b53a258
JulianHolzwarth <legolas1509@googlemail.com>
2019-05-10 17:36:44 +0200

    /cpu/esp32/include/freertos/semphr.h: comments added
    some functions do not work like in freertos. Threrefore added comments.
    for example: Giving a Semaphore,that was taken by another thread, will work. (does not work in freertos)

--- cpu/esp32/include/freertos/semphr.h
+++ cpu/esp32/include/freertos/semphr.h
@@ -1,11 +1,27 @@
 /*
- * Copyright (C) 2018 Gunar Schorcht
+ * Copyright (C) 2018 Gunar Schorcht,
+ * Copyright (C) 2019 Freie Universitaet Berlin
  *
  * This file is subject to the terms and conditions of the GNU Lesser
  * General Public License v2.1. See the file LICENSE in the top level
  * directory for more details.
  *
+ */
+
+/**
+ * @ingroup     sys_freertos
+ * @{
+ *
+ * @file
+ * @brief       Implementation of freertos semaphores
+ *
  * FreeRTOS to RIOT-OS adaption module for source code compatibility
+ * Implementation of freertos mutex, recursive mutex and binary semaphore.
+ * Each semaphore has the function create, delete, take and give.
+ *
+ * @author      Gunar Schorcht
+ * @author      Julian Holzwarth <julian.holzwarth@fu-berlin.de>
+ *
  */
 
 #ifndef FREERTOS_SEMPHR_H
@@ -22,24 +38,83 @@
 extern "C" {
 #endif
 
-typedef void* SemaphoreHandle_t;
+typedef void *SemaphoreHandle_t;
 
+/**
+ * @brief   creates a mutex semaphore
+ *
+ * @return  the handle of the semaphore
+ */
 SemaphoreHandle_t xSemaphoreCreateMutex(void);
+
+/**
+ * @brief   creates a recursive mutex semaphore
+ *
+ * @return  the handle of the semaphore
+ */
 SemaphoreHandle_t xSemaphoreCreateRecursiveMutex(void);
 
-void vSemaphoreDelete (SemaphoreHandle_t xSemaphore);
+/**
+ * @brief   deletes a semaphore
+ * 
+ * @param   xSemaphore  the semaphore handle
+ */
+void vSemaphoreDelete(SemaphoreHandle_t xSemaphore);
 
-BaseType_t xSemaphoreGive (SemaphoreHandle_t xSemaphore);
-BaseType_t xSemaphoreTake (SemaphoreHandle_t xSemaphore,
-                           TickType_t xTicksToWait);
-BaseType_t xSemaphoreGiveRecursive (SemaphoreHandle_t xSemaphore);
-BaseType_t xSemaphoreTakeRecursive (SemaphoreHandle_t xSemaphore,
-                                    TickType_t xTicksToWait);
+/**
+ * @brief   gives (or releases) the semaphore
+ *          can be used to give (release) the
+ *          semaphore even if the calling thread
+ *          did not take it
+ *
+ * @param   xSemaphore  the semaphore handle
+ *
+ * @return  pdPass  if  successful
+ * @return  pdFail  else
+ */
+BaseType_t xSemaphoreGive(SemaphoreHandle_t xSemaphore);
+
+/**
+ * @brief   takes the semaphore
+ *
+ * @param   xSemaphore      the semaphore handle
+ * @param   xTicksToWait    not correctly used (if != 0: wait until free)
+ *
+ * @return  pdPass  if  successful
+ * @return  pdFail  else
+ */
+BaseType_t xSemaphoreTake(SemaphoreHandle_t xSemaphore,
+                          TickType_t xTicksToWait);
+
+/**
+ * @brief   gives (or releases) the recrusive semaphore
+ *          can be used to give (release) the
+ *          semaphore even if the calling thread
+ *          did not take it
+ *
+ * @param   xSemaphore      the semaphore handle
+ *
+ * @return  pdPass  if  successful
+ * @return  pdFail  else
+ */
+BaseType_t xSemaphoreGiveRecursive(SemaphoreHandle_t xSemaphore);
+
+/**
+ * @brief   takes the recursive semaphore
+ *
+ * @param   xSemaphore      the semaphore handle
+ * @param   xTicksToWait    not correctly used (if != 0: wait until free)
+ *
+ * @return  pdPass  if  successful
+ * @return  pdFail  else
+ */
+BaseType_t xSemaphoreTakeRecursive(SemaphoreHandle_t xSemaphore,
+                                   TickType_t xTicksToWait);
 
 #define vPortCPUInitializeMutex(m)  mutex_init(m)
 
-void vPortCPUAcquireMutex (portMUX_TYPE *mux);
-void vPortCPUReleaseMutex (portMUX_TYPE *mux);
+void vPortCPUAcquireMutex(portMUX_TYPE *mux);
+void vPortCPUReleaseMutex(portMUX_TYPE *mux);
 
 /*
  * PLESE NOTE: Following definitions were copied directly from the FreeRTOS
@@ -77,5 +152,5 @@ void vPortCPUReleaseMutex (portMUX_TYPE *mux);
 }
 #endif
 
-#endif /* DOXYGEN */
-#endif /* FREERTOS_SEMPHR_H */
+#endif  /* DOXYGEN */
+#endif  /* FREERTOS_SEMPHR_H */
